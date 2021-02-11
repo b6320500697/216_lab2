b0beb157cfb04c22ce8410138afb1f78cabe39ce
b6320500697 <78525157+NewAlisa@users.noreply.github.com>
2021-02-11 16:48:51 +0700

    pro

--- 6320500697-2.c
+++ 6320500697-2.c
@@ -1,7 +1,7 @@
 #include <math.h>
 int main()
 {
-    float time,proA=199.00,proB=299.00,pro;
+    float time,pro=0;
     int time1,time2,time3,time4,time5,se,se1;
     char promotion;
     scanf("%c",&promotion);
@@ -14,7 +14,7 @@ int main()
     {
         if(time2<=200)
         {
-            pro=proA;
+            pro=pro+199;
         }
         else
         {
@@ -22,14 +22,14 @@ int main()
             time5=time4*60;
             se=time5+time3;
             se1=(se*3)/60;
-            pro=proA+se1;
+            pro=199+se1;
         }
     }
     else
     {
         if(time2<=400)
         {
-            pro=proB;
+            pro=pro+299;
         }
         else
         {
@@ -38,7 +38,7 @@ int main()
             time5=time4*60;
             se=time5+time3;
             se1=(se*2)/60;
-            pro=proB+se1;
+            pro=299+se1;
         }
     }
     printf("%.2f",pro);
