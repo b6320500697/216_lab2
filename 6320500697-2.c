3d7d02957adfa545d9763663ff1e89a73ac3d360
b6320500697 <78525157+NewAlisa@users.noreply.github.com>
2021-02-11 17:03:32 +0700

    pro

--- 6320500697-2.c
+++ 6320500697-2.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 int main ()
 {
-
- int t1,t2;
- char a;
- float z,y=0;
- scanf ("%c",&a);
- scanf ("%f",&z);
- t1 = z;
- t2 = z*100;
- t2 = t2%100;
-
- if (a == 'A'){
-  y = y+199;
-  if (z>200){
-   t1 = t1-200;
-   y += t1*3;
-   y += t2*3/60.0;
-  }
- }
- else if(a=='B'){
-        y = y+299;
-        if(z>400){
-            t1 = t1-400;
-            y += t1*2;
-            y += t2*2/60.0;
+    int t1,t2;
+    char pro;
+    float time,i=0;
+    scanf ("%c",&pro);
+    scanf ("%f",&time);
+    t1 = time;
+    t2 = time*100;
+    t2 = t2%100;
+    if (pro == 'A')
+    {
+         i = i+199;
+         if (z>200)
+        {
+        t1 = t1-200;
+        i += t1*3;
+        i += t2*3/60.0;
         }
     }
-    printf("%.2f",y);
+    else if(pro =='B')
+    {
+         y = y+299;
+         if(z>400)
+         {
+            t1 = t1-400;
+            i += t1*2;
+            i += t2*2/60.0;
+         }
+    }
+    printf("%.2f",i);
     return 0;
 }
